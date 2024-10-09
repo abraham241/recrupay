6bd6b5daf422275a896b40f2ccba6aaf86abf67c
abraham241 <huguesmboundou@gmail.com>
2024-10-09 18:00:54 +0100

    resposive section hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaPlay } from "react-icons/fa";
 import recrulogo from "../public/images/recrulogo.jpg";
 
 export default function Home() {
   return (
     <>
-      <nav className="flex justify-between py-10 px-20 items-center ">
+      <nav className="flex justify-between py-10 px-10 items-center md:px-20 ">
         <div className="flex justify-between items-center gap-32">
           <Image
             src={recrulogo}
@@ -28,22 +29,33 @@ export default function Home() {
           </button>
         </div>
       </nav>
-
-      <div className=" py-10 px-20">
-        <h1 className="text-5xl font-bold text-center">
-          Le logiciel qu’il vous faut pour <br />
-          gérer <span className="inline-block mb-2">vos</span>{" "}
-          {/* Ajout d'une marge en bas pour espacer */}
-          <span className="text-green-800">
-            recrutements & <br /> formations
-          </span>
-        </h1>
-        <p className="text-center py-5">
-          Un logiciel tout-en-un qui simplifie la gestion des talents, améliore
-          la collaboration et <br /> permet de suivre le rythme dans un
-          environnement toujours plus compétitif. <br /> Révolutionnez la
-          formation et le recrutement de votre entreprise.
-        </p>
+      {/* section hiro */}
+      <div className=" py-10 px-10 md:px20">
+        <div>
+          <h1 className="text-5xl font-bold text-center">
+            Le logiciel qu’il vous faut pour <br />
+            gérer <span className="inline-block mb-2">vos</span>{" "}
+            {/* Ajout d'une marge en bas pour espacer */}
+            <span className="text-green-800">
+              recrutements & <br /> formations
+            </span>
+          </h1>
+          <p className="text-center py-5">
+            Un logiciel tout-en-un qui simplifie la gestion des talents,
+            améliore la collaboration et <br /> permet de suivre le rythme dans
+            un environnement toujours plus compétitif. <br /> Révolutionnez la
+            formation et le recrutement de votre entreprise.
+          </p>
+        </div>
+        <div className="flex justify-center items-center gap-5">
+          <button className="bg-green-800 px-4 py-2 rounded-full text-white">
+            Essayez une démo{" "}
+          </button>
+          <button className="flex justify-center items-center gap-3 border border-1 px-4 py-2 rounded-full">
+            <FaPlay />
+            Voir la video
+          </button>
+        </div>
       </div>
 
       {/*  */}
